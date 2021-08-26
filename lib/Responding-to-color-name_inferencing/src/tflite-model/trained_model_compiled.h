becbffcd7e64e35173fc9a1a98ad0433abbdfc61
Tauno Erik <sedumacre@gmail.com>
2021-08-26 20:31:59 +0300

    new model

--- lib/Responding-to-color-name_inferencing/src/tflite-model/trained_model_compiled.h
+++ lib/Responding-to-color-name_inferencing/src/tflite-model/trained_model_compiled.h
@@ -18,7 +18,7 @@
 * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 * SOFTWARE.
 */
-// Generated on: 25.08.2021 16:19:43
+// Generated on: 26.08.2021 17:19:27
 
 #ifndef trained_model_GEN_H
 #define trained_model_GEN_H
